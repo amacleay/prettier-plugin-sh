ab998e5c06e7996fd5588ea001f0f924b1d550aa
Jérémie Astori <jeremie@astori.fr>
2018-05-28 00:39:17 -0400

    Export languages key required for Prettier plugins

---
+++ src/index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const languages = [
+  {
+    name: "Shell",
+    parsers: ["sh"],
+    extensions: [
+      ".sh",
+      ".bash",
+      ".zsh"
+    ]
+  }
+];
+
+module.exports = {
+  languages
+};
